3c4cc4213a2ba301a34c248a38d91845d04d1f9b
kirstenmyette <kirstenmyette@aol.com>
2017-09-21 18:18:33 -0500

    First Section of Paralax

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+  $('.carousel').carousel();});
+  
+$('.carousel.carousel-slider')
+  .carousel({fullWidth: true});
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+    
